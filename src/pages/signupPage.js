50ed95cd32ed6d7d3a6288a1cd943c699c7663f8
Anand Kumar <anandkumarmca6@gmail.com>
2024-07-28 10:20:11 +0530

    frontend

--- src/pages/signupPage.js
+++ src/pages/signupPage.js
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // If using React Router for navigation
-import useSignup from '../hooks/useSignup';
-import './signupPage.css'; // Import your CSS file
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; // If using React Router for navigation
+import useSignup from "../hooks/useSignup";
+import "./signupPage.css"; // Import your CSS file
 
 const SignupPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { signup } = useSignup();
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+  const validate = () => {
+    let validation = true;
 
-  const handleSubmit = () => {
-    const validation = true; // Replace with actual validation logic
+    if (!email) {
+        alert( "Email is required");
+    } else if (!validateEmail(email)) {
+        validation = true;
+        alert("Email is invalid");
+    }
+
+   
+
+    if (!password) {
+        validation = false;
+        alert( "Password is required");
+    } 
+
+
     if (validation) {
       signup({ email, password });
-      console.log('Signup successful');
-    } else {
-      alert('Validation Failed'); // Modify as needed
+        setEmail("");
+        setPassword("");
     }
-  };
+};
+
 
   return (
     <div className="signup-page">
@@ -34,8 +53,10 @@ const SignupPage = () => {
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Enter your password"
         />
-        <button onClick={handleSubmit}>Sign Up</button>
-        <Link to="/login" className="login-link">Already have an account? Login</Link>
+        <button onClick={validate}>Sign Up</button>
+        <Link to="/login" className="login-link">
+          Already have an account? Login
+        </Link>
       </div>
     </div>
   );
