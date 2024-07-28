6869312c1c78361aa36601d347c74b648426d0b8
Anand Kumar <anandkumarmca6@gmail.com>
2024-07-28 08:27:06 +0530

    frontend

--- src/hooks/useGetFileFolders.js
+++ src/hooks/useGetFileFolders.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { appLogout } from "../store/slices/authSlice";
@@ -8,8 +8,8 @@ const useGetFileFolders = () => {
   const { token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
-  const getFileFolders = async (parentId = null) => {
+
+  const getFileFolders = useCallback(async (parentId = null) => {
     try {
       const res = await fetch(`${process.env.BACKEND_URL}/api/v1/file-folder`, {
         method: "POST",
@@ -20,29 +20,25 @@ const useGetFileFolders = () => {
         },
       });
 
-      // Check for authentication errors
       if (res.status === 401) {
         dispatch(appLogout());
         navigate("/login");
         return;
       }
 
-      // Handle non-successful responses
       if (!res.ok) {
         const errorData = await res.json();
-        console.error('Error details:', errorData.message || 'Unknown error'); // Log only the message
+        console.error('Error details:', errorData.message || 'Unknown error');
         throw new Error(errorData.message || 'Failed to fetch file folders.');
       }
 
-      // Parse the response data
       const data = await res.json();
       setFileFolders(data.data.fileFolders);
 
     } catch (error) {
-      console.error('Error fetching file folders:', error.message); // Log only the message
-      //alert('An error occurred while fetching file folders. Please try again later.');
+      console.error('Error fetching file folders:', error.message);
     }
-  };
+  }, [token, dispatch, navigate]);
 
   return { getFileFolders, fileFolders };
 };
